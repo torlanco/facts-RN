3f97bcd4fec25683ee81a48f20d5437e39bfcbab
Mohit Nagori <nagorimohit21@gmail.com>
2019-06-11 16:48:31 +0530

    Refactored Advertisement GridView

--- src/screens/AdvertisementScreen/components/AdvertisementGridView.tsx
+++ src/screens/AdvertisementScreen/components/AdvertisementGridView.tsx
@@ -1,39 +1,64 @@
 import * as React from 'react';
 import {
     StyleSheet,
-    Dimensions
+    Dimensions,
+    View,
+    ScrollView,
+    ListView
 } from 'react-native';
 import { IAdvertisement } from '@interfaces/advertisement';
 import { AdvertisementGridItem } from './AdvertisementGridItem';
-import { FlatGrid } from 'react-native-super-grid';
 
 interface IOwnProps {
   advertisementList: Array<IAdvertisement.IAdvertisementData>
 }
 type IProps = IOwnProps;
 const AdvertisementGridView: React.SFC<IProps> = (props: IProps) => {
-  const width = (Dimensions.get('window').width >> 1) - 12;
+  
+  const sectionOneAdvertisement = props.advertisementList.filter((item, index) => {
+    if (!(index & 1)) {
+      return item;
+    }
+  });
+
+  const sectionTwoAdvertisement = props.advertisementList.filter((item, index) => {
+    if (index & 1) {
+      return item;
+    }
+  });
+
+  const datasource = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+
   return (
-    <FlatGrid
-      itemDimension={width}
-      spacing={1}
-      items={props.advertisementList}
-      renderItem={({ item, index }) => (<AdvertisementGridItem advertisement={item} key={index}></AdvertisementGridItem>)}
-      style={styles.gridView}
-      itemContainerStyle={styles.itemContainerStyle} />
+    <View style={styles.wrapper}>
+      <ScrollView contentContainerStyle={styles.container}>
+        <ListView 
+            contentContainerStyle={styles.list} 
+            dataSource={datasource.cloneWithRows(sectionOneAdvertisement)} 
+            renderRow={(item) => <AdvertisementGridItem advertisement={item}/>}/>
+        <ListView 
+            contentContainerStyle={styles.list} 
+            dataSource={datasource.cloneWithRows(sectionTwoAdvertisement)}
+            renderRow={(item) => <AdvertisementGridItem advertisement={item}/>}/>
+      </ScrollView>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
-  gridView: {
-    marginTop: 20,
-    flex: 1,
+  wrapper: {
+    flex: 1
   },
-  itemContainerStyle: {
-    justifyContent: 'flex-start',
-    paddingBottom: 10,
-    borderRadius: 10,
+  container: {
+      flexDirection: 'row',
+      paddingHorizontal: 5
   },
+  list: {
+      flex: 1,
+      flexDirection: 'column',
+      paddingVertical: 10,
+      paddingHorizontal: 2.5
+  }
 });
 
 export { AdvertisementGridView };
