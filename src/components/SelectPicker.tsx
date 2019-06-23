f350ef9f17622527a882a59ec7714b1a8e22cf5e
Piyush Beli <piyush.beli@gmail.com>
2019-06-23 12:12:02 +0530

    Use new picker which is consistent on both IOS and Android

--- src/components/SelectPicker.tsx
+++ src/components/SelectPicker.tsx
@@ -2,30 +2,43 @@ import * as React from 'react';
 
 import {
     StyleSheet,
+    Picker,
+    TextInput
 } from 'react-native';
-import { Dropdown } from 'react-native-material-dropdown';
+import { colors, typos } from '@styles';
+import ModalSelector from 'react-native-modal-selector';
 
 interface IOwnProps {
     options: string[] | undefined,
     value: string,
     handleValueChange: Function
 }
-
 type IProps = IOwnProps;
 
 const SelectPicker: React.SFC<IProps> = (props: IProps) => {
-    const data = props.options ? props.options.map(item => {return {value: item}; }) : [];
     return (
-        <Dropdown data={data} value={props.value}
-                  onChangeText={(value) => props.handleValueChange(value)}/>
+        <ModalSelector
+            data={props.options}
+            keyExtractor= {(item: string) => item}
+            labelExtractor= {(item: string) => item}
+            initValue={props.value}
+            onChange={(itemValue: object)=> props.handleValueChange(itemValue)}>
+
+            <TextInput
+                style={{borderWidth:1, borderColor:'#ccc', padding:20, height:20}}
+                editable={false}
+                placeholder="Select an outlet"
+                value={props.value} />
+
+        </ModalSelector>
     );
 };
 
 const styles = StyleSheet.create({
-    picker: {
-        height: 50,
-        width: 200,
-    },
+  picker: {
+    height: 50,
+    width: 200,
+  },
 });
 
 export { SelectPicker };
